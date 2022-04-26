4bce72f0d1861d22b62007092585b6e668097861
tran vinh <tranvinhlx135@gmail.com>
2022-04-26 16:26:11 +0700

    done

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  return;
   res.status(200).render("home", { title: "Bầu Spa - Chăm sóc Mẹ & Bé" });
 });
 
