75026a39bd26d299a1c97bc7d81c16de8d6c37ee
tran vinh <tranvinhlx135@gmail.com>
2022-05-06 16:28:50 +0700

    change service

--- routes/admin.js
+++ routes/admin.js
@@ -2,24 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 // Require controller modules.
-const admin_controller = require("../app/controllers/adminController");
+const adminController = require("../app/controllers/adminController");
 
 /* [GET] register admin. */
-router.get("/register", admin_controller.admin_register_get);
+router.get("/register", adminController.admin_register_get);
 
 /* [POST] register admin. */
-router.post("/register", admin_controller.admin_register_post);
+router.post("/register", adminController.admin_register_post);
 
 /* [GET] login admin. */
-router.get("/login", admin_controller.admin_login_get);
+router.get("/login", adminController.admin_login_get);
 
 /* [POST] login admin. */
-router.post("/login", admin_controller.admin_login_post);
+router.post("/login", adminController.admin_login_post);
 
 /* [GET] logout admin. */
-router.get("/logout", admin_controller.admin_logout_get);
-
-/* [GET] dashboard. */
-router.get("/dashboard", admin_controller.admin_dashboard);
+router.get("/logout", adminController.admin_logout_get);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,16 @@
 const homeRoute = require("./home");
 const introRoute = require("./intro");
 const servicesRoute = require("./services");
-const postsRoute = require("./posts");
-const adminRoute = require("./admin");
-const { checkUser } = require("../app/middlewares/authMiddleware");
+// const postsRoute = require("./posts");
 
 function route(app) {
-  app.use("*", checkUser);
-
   app.use("/", homeRoute);
 
   app.use("/gioi-thieu", introRoute);
 
   app.use("/services", servicesRoute);
 
-  app.use("/posts", postsRoute);
-
-  app.use("/admin", adminRoute);
+  // app.use("/posts", postsRoute);
 }
 
 module.exports = route;
